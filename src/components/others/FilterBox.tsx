206913e49689b220646964937144508e5e1d0447
Ibrahim Sule <ibraheemsulay@gmail.com>
2022-10-15 21:22:01 +0100

    fixed the select dropdown bug

--- src/components/others/FilterBox.tsx
+++ src/components/others/FilterBox.tsx
@@ -13,7 +13,7 @@ import {
 const FilterBox: React.FC<IFilterBoxProps> = props => {
   const { title, sortItems, filter, setFilter, sort, setSort } = props;
   const dispatch = useAppDispatch();
-  const dropdown = useRef<HTMLButtonElement | null>(null);
+  const [dropdown, setDropdown] = useState(false);
 
   useEffect(() => {
     if (title === "order")
@@ -63,28 +63,32 @@ const FilterBox: React.FC<IFilterBoxProps> = props => {
 
   const sortValue = (e: any) => {
     setSort && setSort(e.target.outerText);
-    dropdown.current?.blur();
   };
 
   return (
     <fieldset className={s.filter_box}>
       <legend className={s.float}>{title}</legend>
-      <button ref={dropdown}>
-        <div className={s.wrapper}>
-          {title === "category" && <span>{sort || sortItems[0]}</span>}
-          {title === "order" && <span>{sort || "default"}</span>}
-          {title === "date" && <span>{sort || "default"}</span>}
-          <img src={arrowDownIcon} alt="search icon" />
-        </div>
+      <div onBlur={() => setDropdown(false)} tabIndex={-1}>
+        <button
+          onClick={() => setDropdown(!dropdown)}
+          className={`${dropdown ? s.dropdown_active : ""}`}
+        >
+          <div className={s.wrapper}>
+            {title === "category" && <span>{sort || sortItems[0]}</span>}
+            {title === "order" && <span>{sort || "default"}</span>}
+            {title === "date" && <span>{sort || "default"}</span>}
+            <img src={arrowDownIcon} alt="search icon" />
+          </div>
 
-        <ul className={s.dropdown}>
-          {sortItems.map((item, i) => (
-            <li key={i} onClick={(e: any) => sortValue(e)}>
-              {item}
-            </li>
-          ))}
-        </ul>
-      </button>
+          <ul className={s.dropdown}>
+            {sortItems.map((item, i) => (
+              <li key={i} onClick={(e: any) => sortValue(e)}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </button>
+      </div>
     </fieldset>
   );
 };
